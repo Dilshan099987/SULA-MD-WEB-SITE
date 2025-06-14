16b88b612912be07d08df971abaedb38bf9a504f
Dilshan099987 <ashinsadilshan494@gmail.com>
2025-06-14 20:50:38 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -13,7 +13,7 @@ import * as mega from 'megajs';
 // Mega authentication credentials
 const auth = {
     email: 'dilshanashinsa258@gmail.com', // Replace with your Mega email
-    password: 'FKVwqg8CSbLe_87', // Replace with your Mega password
+    password: 'zWh.t2u59-SxLn7', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
